5d07a557818a013b7e69716987e59bec9066a8ac
DELL <tarun.kachhela2000@gmail.com>
2019-12-08 03:45:44 +0530

    work done

---
+++ Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace Intrusion_Detection_System
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.watchBtn = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.watchFile = new System.Windows.Forms.RadioButton();
+            this.watchFolder = new System.Windows.Forms.RadioButton();
+            this.includeSubCheckbox = new System.Windows.Forms.CheckBox();
+            this.browseBtn = new System.Windows.Forms.Button();
+            this.logTextbox = new System.Windows.Forms.RichTextBox();
+            this.pathTextBox = new System.Windows.Forms.RichTextBox();
+            this.dumpBtn = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // watchBtn
+            // 
+            this.watchBtn.BackColor = System.Drawing.SystemColors.Control;
+            this.watchBtn.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.watchBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.watchBtn.Location = new System.Drawing.Point(30, 194);
+            this.watchBtn.Margin = new System.Windows.Forms.Padding(0);
+            this.watchBtn.Name = "watchBtn";
+            this.watchBtn.Size = new System.Drawing.Size(95, 24);
+            this.watchBtn.TabIndex = 2;
+            this.watchBtn.Text = "Watch";
+            this.watchBtn.UseVisualStyleBackColor = false;
+            this.watchBtn.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.watchFile);
+            this.groupBox1.Controls.Add(this.watchFolder);
+            this.groupBox1.Controls.Add(this.includeSubCheckbox);
+            this.groupBox1.ForeColor = System.Drawing.Color.Blue;
+            this.groupBox1.Location = new System.Drawing.Point(30, 24);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(243, 84);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Options";
+            // 
+            // watchFile
+            // 
+            this.watchFile.AutoSize = true;
+            this.watchFile.ForeColor = System.Drawing.Color.Black;
+            this.watchFile.Location = new System.Drawing.Point(6, 19);
+            this.watchFile.Name = "watchFile";
+            this.watchFile.Size = new System.Drawing.Size(81, 17);
+            this.watchFile.TabIndex = 1;
+            this.watchFile.TabStop = true;
+            this.watchFile.Text = "Watch Files";
+            this.watchFile.UseVisualStyleBackColor = true;
+            this.watchFile.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // watchFolder
+            // 
+            this.watchFolder.AutoSize = true;
+            this.watchFolder.ForeColor = System.Drawing.Color.Black;
+            this.watchFolder.Location = new System.Drawing.Point(6, 50);
+            this.watchFolder.Name = "watchFolder";
+            this.watchFolder.Size = new System.Drawing.Size(89, 17);
+            this.watchFolder.TabIndex = 1;
+            this.watchFolder.TabStop = true;
+            this.watchFolder.Text = "Watch Folder";
+            this.watchFolder.UseVisualStyleBackColor = true;
+            // 
+            // includeSubCheckbox
+            // 
+            this.includeSubCheckbox.AutoSize = true;
+            this.includeSubCheckbox.ForeColor = System.Drawing.Color.Black;
+            this.includeSubCheckbox.Location = new System.Drawing.Point(106, 50);
+            this.includeSubCheckbox.Name = "includeSubCheckbox";
+            this.includeSubCheckbox.Size = new System.Drawing.Size(131, 17);
+            this.includeSubCheckbox.TabIndex = 0;
+            this.includeSubCheckbox.Text = "Include Subdirectories";
+            this.includeSubCheckbox.UseVisualStyleBackColor = true;
+            this.includeSubCheckbox.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // browseBtn
+            // 
+            this.browseBtn.Location = new System.Drawing.Point(299, 144);
+            this.browseBtn.Name = "browseBtn";
+            this.browseBtn.Size = new System.Drawing.Size(96, 27);
+            this.browseBtn.TabIndex = 4;
+            this.browseBtn.Text = "Browse";
+            this.browseBtn.UseVisualStyleBackColor = true;
+            this.browseBtn.Click += new System.EventHandler(this.Browse_Click);
+            // 
+            // logTextbox
+            // 
+            this.logTextbox.Location = new System.Drawing.Point(29, 262);
+            this.logTextbox.Name = "logTextbox";
+            this.logTextbox.Size = new System.Drawing.Size(366, 151);
+            this.logTextbox.TabIndex = 5;
+            this.logTextbox.Text = "";
+            // 
+            // pathTextBox
+            // 
+            this.pathTextBox.EnableAutoDragDrop = true;
+            this.pathTextBox.Location = new System.Drawing.Point(29, 151);
+            this.pathTextBox.Name = "pathTextBox";
+            this.pathTextBox.Size = new System.Drawing.Size(243, 19);
+            this.pathTextBox.TabIndex = 6;
+            this.pathTextBox.Text = "";
+            // 
+            // dumpBtn
+            // 
+            this.dumpBtn.Location = new System.Drawing.Point(36, 442);
+            this.dumpBtn.Name = "dumpBtn";
+            this.dumpBtn.Size = new System.Drawing.Size(107, 25);
+            this.dumpBtn.TabIndex = 7;
+            this.dumpBtn.Text = "Dump To Log";
+            this.dumpBtn.UseVisualStyleBackColor = true;
+            this.dumpBtn.Click += new System.EventHandler(this.dumpBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(456, 493);
+            this.Controls.Add(this.dumpBtn);
+            this.Controls.Add(this.pathTextBox);
+            this.Controls.Add(this.logTextbox);
+            this.Controls.Add(this.browseBtn);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.watchBtn);
+            this.Name = "Form1";
+            this.Text = "FileWatcher";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button watchBtn;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox includeSubCheckbox;
+        private System.Windows.Forms.RadioButton watchFolder;
+        private System.Windows.Forms.RadioButton watchFile;
+        private System.Windows.Forms.Button browseBtn;
+        private System.Windows.Forms.RichTextBox logTextbox;
+        private System.Windows.Forms.RichTextBox pathTextBox;
+        private System.Windows.Forms.Button dumpBtn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Intrusion_Detection_System
+{
+    public partial class Form1 : Form
+    {
+        FileSystemWatcher watcher;
+        bool isWatchSelect;
+        string log = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(pathTextBox.Text == "")
+            {
+                MessageBox.Show("Please Select a path", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+
+                if (!isWatchSelect)
+                {
+                    watchBtn.BackColor = Color.Red;
+                    watcher = new FileSystemWatcher();
+                    watcher.Path = pathTextBox.Text;
+                    //For Testing
+                    //MessageBox.Show("I am in" + pathTextBox.Text);
+                    watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite
+                    | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+                    watcher.EnableRaisingEvents = true;
+                    watcher.Changed += new FileSystemEventHandler(OnChanged);
+                    watcher.Created += new FileSystemEventHandler(OnCreated);
+                    watcher.Deleted += new FileSystemEventHandler(OnDeleted);
+                    watcher.Renamed += new RenamedEventHandler(OnRenamed);
+                    watcher.Error += new ErrorEventHandler(OnError);
+                    isWatchSelect = true;
+                }
+                else
+                {
+                    watchBtn.BackColor = default(Color);
+                    watcher.EnableRaisingEvents = false;
+                    watcher.Dispose();
+                    isWatchSelect = false;
+                }
+            }
+        }
+
+
+        private void OnCreated (object sender, FileSystemEventArgs e)
+        {
+            log += " Created " + e.FullPath + " " + e.ChangeType + DateTime.Now + "\n";
+            logTextbox.Invoke(new Action(() => logTextbox.Text = log));
+
+            //logTextbox.Text = log;
+        }
+        private void OnDeleted(object sender, FileSystemEventArgs e)
+        {
+            log+= " Deleted " + e.FullPath + " " + e.ChangeType + DateTime.Now+"\n";
+            logTextbox.Invoke(new Action(() => logTextbox.Text = log));
+        }
+
+        private void OnChanged(object sender, FileSystemEventArgs e)
+        {
+
+            WatcherChangeTypes wct = e.ChangeType;
+            log += "File Changed" + e.FullPath + " " + wct.ToString() + DateTime.Now + "\n";
+            logTextbox.Invoke(new Action(() => logTextbox.Text = log));
+
+
+        }
+        private void OnRenamed(object sender, RenamedEventArgs e)
+        {
+            WatcherChangeTypes wct = e.ChangeType;
+            log += " Renamed " + e.OldFullPath + " " + e.FullPath + " " + wct.ToString() + DateTime.Now + "\n";
+            logTextbox.Invoke(new Action(() => logTextbox.Text = log));
+
+        }
+        private void OnError(object sender, ErrorEventArgs e)
+        {
+            //  Show that an error has been detected.
+            MessageBox.Show("The FileSystemWatcher has detected an error", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            //  Give more information if the error is due to an internal buffer overflow.
+            if (e.GetException().GetType() == typeof(InternalBufferOverflowException))
+            {
+                //  This can happen if Windows is reporting many file system events quickly 
+                //  and internal buffer of the  FileSystemWatcher is not large enough to handle this
+                //  rate of events. The InternalBufferOverflowException error informs the application
+                //  that some of the file system events are being lost.
+                MessageBox.Show(("The file system watcher experienced an internal buffer overflow: " + e.GetException().Message),"Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            isWatchSelect = false;
+            watcher = new FileSystemWatcher();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(includeSubCheckbox.Checked == true)
+            {
+                watcher.IncludeSubdirectories = true;
+            }
+            watcher.IncludeSubdirectories = false;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+           
+            
+        }
+
+        private void Browse_Click(object sender, EventArgs e)
+        {
+            if (watchFolder.Checked == true)
+            {
+                FolderBrowserDialog fbd = new FolderBrowserDialog();
+                fbd.RootFolder = Environment.SpecialFolder.Desktop;
+                fbd.Description = "Select a folder";
+                fbd.ShowNewFolderButton = true;
+                if (fbd.ShowDialog() == DialogResult.OK)
+                {
+                    pathTextBox.Text = fbd.SelectedPath;
+                }
+            }
+            else if (watchFile.Checked == true) {
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                openFileDialog.Title = "Select a file";
+                openFileDialog.InitialDirectory = @"c:\";
+                openFileDialog.Filter = "All files (*.*)|*.*|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 2;
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    pathTextBox.Text = openFileDialog.FileName;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select an option","Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+        }
+
+        private void dumpBtn_Click(object sender, EventArgs e)
+        {
+            if (logTextbox.Text != "")
+            {
+                string path = Environment.CurrentDirectory + "\\FileWatcherLog.txt";
+                if (!File.Exists(path))
+                {
+                    // Create a file to write to.
+                    using (StreamWriter sw = File.CreateText(path))
+                    {
+
+                        log += "-----------------------------------------------------------------------\n";
+                        sw.WriteLine(log);
+                        log = "";
+                        MessageBox.Show("Log file is created at " + path + " location");
+                    }
+                }
+                else if (File.Exists(path))
+                {
+                    using (var tw = new StreamWriter(path, true))
+                    {
+                        log += "--------------------------------------------------------------------------\n";
+                        tw.WriteLine(log);
+                        log = "";
+                        MessageBox.Show("Log file is created at " + path + " location");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Error while creating file ", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+    }
+}
